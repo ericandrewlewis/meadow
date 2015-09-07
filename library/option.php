af24db1379922a2b25a4879919b47c461fdf225f
Eric Andrew Lewis <eric.andrew.lewis@gmail.com>
2015-09-07 13:29:45 -0400

    echo duh.

--- library/option.php
+++ library/option.php
@@ -30,10 +30,15 @@ class Meadow_Option_UI_Control {
 	}
 
 	public function admin_init() {
+		/*
+		 * Manually register the setting in the "options whitelist" which handles
+		 * saving the data on an options page.
+		 *
+		 * This is an alternative to calling register_setting() which does too much.
+		 */
 		global $new_whitelist_options;
-		$new_whitelist_options[ $this->meta->section ][] = $this->meta->key;
+		$new_whitelist_options[ $this->meta->page ][] = $this->meta->key;
 
-		// register_setting( $this->meta->section, $this->meta->key );
 		// Add the thing that will output the field in the Settings page form.
 		add_settings_field(
 			$this->meta->key,
@@ -47,7 +52,7 @@ class Meadow_Option_UI_Control {
 	public function render() {
 		$value = get_option( $this->meta->key );
 		if ( $this->meta->type === 'text' ) {
-			?><input type="text" name="<?php echo $this->meta->key ?>" value="<?php esc_attr( $value ) ?>"><?php
+			?><input type="text" name="<?php echo $this->meta->key ?>" value="<?php echo esc_attr( $value ) ?>"><?php
 		}
 	}
 }
