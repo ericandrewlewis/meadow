48edcdcb100662bbb2e5ac4ecf9190ae0484517f
Eric Andrew Lewis <eric.andrew.lewis@gmail.com>
2015-09-11 03:03:14 -0400

    Pinking shears

--- examples/option.php
+++ examples/option.php
@@ -2,29 +2,35 @@
 meadow_register_meta(
 	array(
 		/*
-		 * Asset types are recursive, use ":" as a separator to keep this syntax brief.
+		 * Posts, comments, users, settings are top-level asset types.
 		 *
-		 * Posts, comments, users, settings are top-level citizens.
-		 * Narrow down from there based on the respective schemas.
-		 * Open up lower-level filters for more granular control.
+		 * Narrow down from there with 'post_type'.
 		 */
 		'asset_type' => 'option',
+
 		// The key of the meta data.
 		'key' => 'disallow_something',
+
 		/*
 		 * edit_post is always required for postmeta, @see map_meta_cap() and think
 		 * about this some more. It's awkard that __return_true wouldn't work for any
 		 * user here.
 		 */
 		'authentication_callback' => '__return_true',
+
 		// Data sanitization callback.
 		'sanitization_callback' => '__noop_sanitizer',
+
 		// The "type" of the data. This describes what kind of UI to offer the user.
 		'type' => 'text',
+
 		// Label for the UI control.
 		'label' => __( 'Disallow Something' ),
 
+		// The settings page for the control.
 		'page' => 'general',
+
+		// The settings section for the control.
 		'section' => 'default',
 	)
 );
--- examples/post.php
+++ examples/post.php
@@ -2,32 +2,39 @@
 meadow_register_meta(
 	array(
 		/*
-		 * Asset types are recursive, use ":" as a separator to keep this syntax brief.
+		 * Posts, comments, users, settings are top-level asset types.
 		 *
-		 * Posts, comments, users, settings are top-level citizens.
-		 * Narrow down from there based on the respective schemas.
-		 * Open up lower-level filters for more granular control.
+		 * Narrow down from there with 'post_type'.
 		 */
 		'asset_type' => 'post',
+
 		'post_type' => 'post',
+
 		// The key of the meta data.
 		'key' => 'subtitle',
+
 		/*
 		 * edit_post is always required for postmeta, @see map_meta_cap() and think
 		 * about this some more. It's unintuitive that __return_true wouldn't work if the
 		 * user couldn't edit the post.
 		 */
 		'authentication_callback' => '__return_true',
+
 		/*
 		 * Data sanitization callback.
 		 */
 		'sanitization_callback' => '__noop_sanitizer',
+
 		// The "type" of the data. This describes what kind of UI to offer the user.
 		'type' => 'text',
+
 		// Label for the UI control.
 		'label' => __( 'Subtitle' ),
-		// Where the field should render on the Edit Post screen. either 'under_title',
-		// 'post_submitbox_misc_actions', or somehow a meta box.
+
+		/*
+		 * Where the field should render on the Edit Post screen. either 'under_title',
+		 * 'post_submitbox_misc_actions', or somehow a meta box.
+		 */
 		'edit_post_location' => 'post_submitbox_misc_actions',
 	)
 );
